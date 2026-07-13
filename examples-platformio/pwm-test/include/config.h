f5ee88536197e5fcaec8b2fd6763c3260b6f71f0
Fo170 <fo170@github.com>
2026-07-13 20:42:00 +0200

    Fix pwm-test config.h: align PIC18F4550 pragmas with serial-echo (no more 1311 warnings)

--- examples-platformio/pwm-test/include/config.h
+++ examples-platformio/pwm-test/include/config.h
@@ -13,30 +13,13 @@
 #elif defined(__18F4550)
   #pragma config FOSC = HSPLL_HS
   #pragma config WDT = OFF
-  #pragma config PBADEN = OFF
-  #pragma config LPT1OSC = OFF
-  #pragma config MCLRE = ON
-  #pragma config STVREN = ON
-  #pragma config LVP = OFF
-  #pragma config XINST = OFF
   #pragma config CPUDIV = OSC1_PLL2
-  #pragma config CP0 = OFF
-  #pragma config CP1 = OFF
-  #pragma config CP2 = OFF
-  #pragma config CP3 = OFF
-  #pragma config CPB = OFF
-  #pragma config CPD = OFF
-  #pragma config WRT0 = OFF
-  #pragma config WRT1 = OFF
-  #pragma config WRT2 = OFF
-  #pragma config WRT3 = OFF
-  #pragma config WRTC = OFF
-  #pragma config WRTB = OFF
-  #pragma config WRTD = OFF
-  #pragma config EBTR0 = OFF
-  #pragma config EBTR1 = OFF
-  #pragma config EBTR2 = OFF
-  #pragma config EBTR3 = OFF
+  #pragma config USBDIV = 2
+  #pragma config VREGEN = ON
+  #pragma config PBADEN = OFF
+  #pragma config LVP = OFF
+  #pragma config MCLRE = ON
+  #pragma config XINST = OFF
   #define _XTAL_FREQ 48000000
 #endif
 
